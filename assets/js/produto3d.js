f9dd0c1ad0c8701bd03613abdaa5437d702bf434
jeffersontads <jeffersontads@hotmail.com>
2024-06-19 21:08:36 -0300

    substitui meta name viewport

--- assets/js/produto3d.js
+++ assets/js/produto3d.js
@@ -29,28 +29,87 @@ var mainSwiper = new Swiper(".mySwiper", {
       }
     });
   }
-  document.querySelectorAll('.main-image').forEach(function(image) {
-    // Check for touch support to enable mobile zoom
-    if ('ontouchstart' in window) {
-      image.addEventListener('touchstart', function(event) {
-        event.preventDefault(); // Prevent default behavior (scrolling) on touchstart
+
+        // Add fullscreen functionality
+        document.querySelectorAll('.main-image').forEach(function(image) {
+          image.addEventListener('click', function() {
+              if (image.requestFullscreen) {
+                  image.requestFullscreen();
+              } else if (image.mozRequestFullScreen) { // Firefox
+                  image.mozRequestFullScreen();
+              } else if (image.webkitRequestFullscreen) { // Chrome, Safari, Opera
+                  image.webkitRequestFullscreen();
+              } else if (image.msRequestFullscreen) { // IE/Edge
+                  image.msRequestFullscreen();
+              }
+          });
       });
-    }
-  
-    // Fullscreen functionality (remains the same)
-    image.addEventListener('click', function() {
-      if (image.requestFullscreen) {
-        image.requestFullscreen();
-      } else if (image.mozRequestFullScreen) { // Firefox
-        image.mozRequestFullScreen();
-      } else if (image.webkitRequestFullscreen) { // Chrome, Safari, Opera
-        image.webkitRequestFullscreen();
-      } else if (image.msRequestFullscreen) { // IE/Edge
-        image.msRequestFullscreen();
+
+      document.addEventListener('fullscreenchange', function() {
+          var fullscreenElement = document.fullscreenElement ||
+                                  document.webkitFullscreenElement ||
+                                  document.mozFullScreenElement ||
+                                  document.msFullscreenElement;
+
+          var images = document.querySelectorAll('.main-image');
+          images.forEach(function(image) {
+              if (image === fullscreenElement) {
+                  enableZoomAndPan(image);
+              } else {
+                  disableZoomAndPan(image);
+              }
+          });
+      });
+
+      function enableZoomAndPan(image) {
+          image.classList.add('fullscreen-image');
+          image.addEventListener('touchstart', handleTouchStart, false);
+          image.addEventListener('touchmove', handleTouchMove, false);
       }
-    });
-  });
-  
+
+      function disableZoomAndPan(image) {
+          image.classList.remove('fullscreen-image');
+          image.removeEventListener('touchstart', handleTouchStart, false);
+          image.removeEventListener('touchmove', handleTouchMove, false);
+      }
+
+      // Variables to keep track of touch points for zoom and pan
+      var initialDistance = 0;
+      var initialScale = 1;
+      var initialX = 0;
+      var initialY = 0;
+
+      function handleTouchStart(event) {
+          if (event.touches.length === 1) {
+              initialX = event.touches[0].clientX;
+              initialY = event.touches[0].clientY;
+          } else if (event.touches.length === 2) {
+              initialDistance = getDistance(event.touches[0], event.touches[1]);
+              initialScale = parseFloat(event.target.style.transform.replace(/[^0-9.]/g, '')) || 1;
+          }
+      }
+
+      function handleTouchMove(event) {
+          if (event.touches.length === 1) {
+              var currentX = event.touches[0].clientX;
+              var currentY = event.touches[0].clientY;
+              var deltaX = currentX - initialX;
+              var deltaY = currentY - initialY;
+              event.target.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
+              event.preventDefault();
+          } else if (event.touches.length === 2) {
+              var newDistance = getDistance(event.touches[0], event.touches[1]);
+              var scale = initialScale * (newDistance / initialDistance);
+              event.target.style.transform = `scale(${scale})`;
+              event.preventDefault();
+          }
+      }
+
+      function getDistance(touch1, touch2) {
+          var dx = touch1.pageX - touch2.pageX;
+          var dy = touch1.pageY - touch2.pageY;
+          return Math.sqrt(dx * dx + dy * dy);
+      }
   
     // Exit fullscreen when fullscreen mode is closed
     document.addEventListener('fullscreenchange', function() {
--- productos_espanhol/empanada_de_carne_al_horno.php
+++ productos_espanhol/empanada_de_carne_al_horno.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8" />
   <title>Empanadas de Carne al Horno - Misslaura</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- Link Swiper's CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
   <link rel="stylesheet" href="../assets/css/produto3d.css" />
